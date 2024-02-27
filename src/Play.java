127b5a7ea43a3466dc2e0b1ca4e0084f2f618c04
Rai Bizerra Maciel <raibizerramaciel@gmail.com>
2024-02-27 20:32:54 -0300

    Reorganizing Play class

--- src/Play.java
+++ src/Play.java
@@ -4,18 +4,25 @@ import functionalInterface.MyFunctionalInterface;
 import java.util.function.Predicate;
 
 public class Play {
-
     public static void main(String[] args) {
+        Play p = new Play();
+        p.PLAY_functionalInterface();
+        p.PLAY_predicateWithLambda();
+    }
+
+    public void PLAY_functionalInterface() {
         MyFunctionalInterface m = () -> System.out.println("Hello World!");
+        IsValidText<String> validator = t -> t != null && !t.isEmpty();
+
         m.function();
 
         String text = "Frog";
-        String text2 = null;
-        IsValidText<String> validator = t -> t != null && !t.isEmpty();
-
         System.out.println("Text is valid? "+validator.function(text));
-        System.out.println("Text 2 is valid? "+validator.function(text2));
+        text = null;
+        System.out.println("Text 2 is valid? "+validator.function(text));
+    }
 
+    public void PLAY_predicateWithLambda() {
         String name = "Fire God";
         System.out.println("Is "+name+" an elemental god? "+ check(name, n -> n.endsWith("God")));
         name = "Fire Lord";
