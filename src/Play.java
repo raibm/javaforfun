5de385c5f9b3161163e35513dede679f4f776eb8
Rai Bizerra Maciel <raibizerramaciel@gmail.com>
2024-02-27 20:20:27 -0300

    Simple example with Predicate

--- src/Play.java
+++ src/Play.java
@@ -1,11 +1,12 @@
-import functionalInterface.Check;
 import functionalInterface.IsValidText;
 import functionalInterface.MyFunctionalInterface;
+
+import java.util.function.Predicate;
 
 public class Play {
 
     public static void main(String[] args) {
-        MyFunctionalInterface m = () -> {System.out.println("Hello World!");};
+        MyFunctionalInterface m = () -> System.out.println("Hello World!");
         m.function();
 
         String text = "Frog";
@@ -15,6 +16,13 @@ public class Play {
         System.out.println("Text is valid? "+validator.function(text));
         System.out.println("Text 2 is valid? "+validator.function(text2));
 
-        
+        String name = "Fire God";
+        System.out.println("Is "+name+" an elemental god? "+ check(name, n -> n.endsWith("God")));
+        name = "Fire Lord";
+        System.out.println("Is "+name+" an elemental god? "+ check(name, n -> n.endsWith("God")));
+    }
+
+    public static <T> boolean check(T t, Predicate<T> lambda) {
+        return lambda.test(t);
     }
 }
--- src/functionalInterface/Check.java
+++
@@ -1,8 +0,0 @@
-package functionalInterface;
-
-import java.util.function.Predicate;
-
-@FunctionalInterface
-public interface Check<T> {
-    boolean f(T t, Predicate<T> lambda);
-}
