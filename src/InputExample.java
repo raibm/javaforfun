1e14d98b1c60919594e4c37da2642bd7c9a91eb5
Rai Bizerra Maciel <raibizerramaciel@gmail.com>
2024-03-01 22:04:30 -0300

    Just a simples Scanner for input

---
+++ src/InputExample.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class InputExample {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Integer number = input.nextInt();
+        System.out.println("Your number is: "+number);
+    }
+}
